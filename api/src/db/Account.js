d11b2635e858776d57e623123b545d2956d4dad7
Lori Culberson <lori_culberson@homedepot.com>
2019-06-14 11:28:38 -0400

    Add admin flag to account

--- api/src/db/Account.js
+++ api/src/db/Account.js
@@ -5,8 +5,9 @@ const Schema = mongoose.Schema
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const Account = new Schema({
-  firstName: String,
-  lastName: String
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
+  admin:  { type: Boolean, required: true, default: false }
 })
 
 Account.plugin(passportLocalMongoose)

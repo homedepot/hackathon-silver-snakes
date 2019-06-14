5d3f04b5afb9d47b57610a9c7142d9237437d04f
Lissett Munoz <lissett_munoz@homedepot.com>
2019-06-14 10:48:56 -0400

    Added some routes to start

--- ui/src/app/App.js
+++ ui/src/app/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Login from '../login/Login'
 import Landing from '../landing/Landing'
+import WishCreation from '../wishCreation/WishCreation'
 import WatchAuth from '../auth/WatchAuth'
+import WishSponsorShip from '../wishSponsorship/WishSponsorship'
+import WishCuration from '../wishCuration/WishCuration'
+import Admin from '../admin/Admin'
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <WatchAuth>
         <Switch>
           <Route exact path="/landing" component={Landing} />
+          <Route exact path="/wish-creation" component={WishCreation} />
+          <Route exact path="/wish-curation" component={WishCuration} />
+          <Route exact path="/wish-sponsorship" component={WishSponsorShip} />
+          <Route exact path="/admin" component={Admin} />
           <Route exact path="/" component={Login} />
         </Switch>
       </WatchAuth>
---
+++ ui/src/wishCreation/WishCreation.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+class WishCreation extends Component {
+  render() {
+    return <div>Wish Creation</div>
+  }
+}
+export default WishCreation
